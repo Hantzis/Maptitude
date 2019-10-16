1d5cf2eeac7113f8f8d3419fb3fb9823db18e88f
Nizhi Saeba <mathereall@gmail.com>
2019-10-15 23:23:08 -0500

    inicial

--- webgis/models.py
+++ webgis/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class WGCell(models.Model):
-    cell_geom = models.PolygonField()
+    cell_geom = models.PolygonField(srid=3857)
     cell_user = models.ForeignKey(User, on_delete=models.PROTECT, )
 
 
@@ -15,32 +15,33 @@ class WGProject(models.Model):
     project_users = models.ManyToManyField(User, related_name='users_project')
     project_cellsize = models.PositiveIntegerField(default=1000)
     project_cells = models.ManyToManyField(WGCell, blank=True, null=True)
-    project_geom = models.PolygonField()
+    project_geom = models.PolygonField(srid=3857)
+    project_sridutm = models.PositiveIntegerField(default=32613)
 
     def __str__(self):
         return f'{self.project_name}'
 
 
 class WGPoint(models.Model):
-    point_geom = models.PointField()
+    point_geom = models.PointField(srid=3857)
     point_data = JSONField(blank=True, null=True)
 
 class WGMultiPoint(models.Model):
-    multipoint_geom = models.MultiPointField()
+    multipoint_geom = models.MultiPointField(srid=3857)
     multipoint_data = JSONField(blank=True, null=True)
 
 class WGLineString(models.Model):
-    linestring_geom = models.LineStringField()
+    linestring_geom = models.LineStringField(srid=3857)
     linestring_data = JSONField(blank=True, null=True)
 
 class WGMultiLineString(models.Model):
-    multilinestring_geom = models.MultiLineStringField()
+    multilinestring_geom = models.MultiLineStringField(srid=3857)
     multilinestring_data = JSONField(blank=True, null=True)
 
 class WGPolygon(models.Model):
-    polygon_geom = models.MultiPolygonField()
+    polygon_geom = models.MultiPolygonField(srid=3857)
     polygon_data = JSONField(blank=True, null=True)
 
 class WGMultiPolygon(models.Model):
-    multipolygon_geom = models.MultiPolygonField()
+    multipolygon_geom = models.MultiPolygonField(srid=3857)
     multipolygon_data = JSONField(blank=True, null=True)
